146e1ffc9d02f06f25c33ebe012bc65929fb81a1
Ducnt <ducconit@gmail.com>
2021-08-15 07:22:58 +0000

    update readme and change path install package

--- src/Installer.php
+++ src/Installer.php
@@ -6,17 +6,38 @@ use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
 
+if (!function_exists('finish')) {
+    function finish($value, $cap)
+    {
+        $quoted = preg_quote($cap, '/');
+
+        return preg_replace('/(?:' . $quoted . ')+$/u', '', $value) . $cap;
+    }
+}
+
 class Installer extends LibraryInstaller
 {
     public $packageType = 'ducnt-package';
 
+    private $dr = DIRECTORY_SEPARATOR;
+
     public function getInstallPath(PackageInterface $package)
     {
-        return 'resources/themes/' . $package->getPrettyName() ;
+        $extra = $this->composer->getPackage()->getExtra();
+        if (isset($extra['ducnt']) && isset($extra['ducnt']['package-dir'])) {
+            $path = $extra['ducnt']['package-dir'];
+        }
+        $path = finish($path ?? $this->getPathDefault(), $this->dr);
+        return $path . str_replace('/', '-', $package->getPrettyName());
     }
 
     public function supports($packageType)
     {
         return $this->packageType === $packageType;
+    }
+
+    private function getPathDefault()
+    {
+        return 'app' . $this->dr . 'Packages';
     }
 }
